60e4ecb12ed1a2a143fe8ce2c77b5a96e266457c
Sam Wells <sfwells0518@gmail.com>
2023-04-17 14:13:00 -0500

    add users create action (signup)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Dog Routes:
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # User Routes:
+  post "/users" => "users#create"
 end
